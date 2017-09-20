9fb0c1f3a5d89f4dc86e0b84161f1c6aa6006e29
f26401004 <f26401004@gmail.com>
2017-09-20 21:39:22 +0800

    upload the solution of uva10370.

---
+++ uva10370.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int case_num;
+	scanf("%d", &case_num);
+	int i, j, N;
+	double total, percent, result[case_num];
+	for (i = 0 ; i < case_num ; ++i)
+	{
+		total = 0;
+		percent = 0;
+		scanf("%d", &N);
+		int space[N];
+		for (j = 0 ; j < N ; ++j)
+		{
+			scanf("%d", &space[j]);
+			total = total + space[j];
+		}
+		total = total / N;
+		for (j = 0 ; j < N ; ++j)
+		{
+			if (space[j] > total)
+				++percent;
+		}
+		result[i] = percent / N * 100;
+	}
+	for (i = 0 ; i < case_num ; ++i)
+		printf("%.3f\n", result[i]);
+}
